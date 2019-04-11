d9d810e45f05b71e67751d7ab1274a562e5960e3
Jie Lu <lujie1996@hotmail.com>
2019-04-11 16:29:10 -0700

    single node OK

--- client.py
+++ client.py
@@ -1,30 +1,38 @@
 import logging
+import sys
 import grpc
 import storage_service_pb2
 import storage_service_pb2_grpc
 
+
 def get(config, key):
-	with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel('localhost:50051') as channel:
         stub = storage_service_pb2_grpc.KeyValueStoreStub(channel)
         response = stub.Get(storage_service_pb2.GetRequest(key=key))
-     	print(response.message)
+        if response.ret == 1:
+            print(response.value)
+        else:
+            print('Failed!')
+
 
 def put(config, key, value):
-	with grpc.insecure_channel('localhost:50051') as channel:
+    with grpc.insecure_channel('localhost:50051') as channel:
         stub = storage_service_pb2_grpc.KeyValueStoreStub(channel)
         response = stub.Put(storage_service_pb2.PutRequest(key=key, value=value))
-     	print(response.message)
+        if response.ret == 1:
+            print('Success!')
+
 
 if __name__ == '__main__':
     logging.basicConfig()
     config = sys.argv[1]
     operation = sys.argv[2]
     if operation == 'get':
-    	key = sys.argv[3]
-    	get(config, key)
+        key = sys.argv[3]
+        get(config, key)
     elif operation == 'put':
         key = sys.argv[3]
         value = sys.argv[4]
-    	get(config, key, value)
+        put(config, key, value)
     else:
         print("Invalid operation")
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from concurrent import futures
+import time
 import logging
 import grpc
 import storage_service_pb2
@@ -5,16 +7,21 @@ import storage_service_pb2_grpc
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
-class StorageServer(helloworld_pb2_grpc.GreeterServicer):
-	self.storage = {}
 
-	def Get(self, request, context):
-  		if request.key in self.storage:
-  			return storage_service_pb2.GetResponse(value=self.storage[request.key], ret=1)
-  		else:
-  			return storage_service_pb2.GetResponse(value=self.storage[request.key], ret=0)
+class StorageServer(storage_service_pb2_grpc.KeyValueStoreServicer):
+    def __init__(self):
+        self.storage = {}
 
-	def Put(self, request, context):
+    def Get(self, request, context):
+        if request.key in self.storage:
+            return storage_service_pb2.GetResponse(value=str(self.storage[request.key]), ret=1)
+        else:
+            return storage_service_pb2.GetResponse(ret=0)
+
+    def Put(self, request, context):
+        self.storage[request.key]=request.value
+        return storage_service_pb2.PutResponse(ret=1)
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -26,6 +33,7 @@ def serve():
             time.sleep(_ONE_DAY_IN_SECONDS)
     except KeyboardInterrupt:
         server.stop(0)
+
 
 if __name__ == '__main__':
     logging.basicConfig()
