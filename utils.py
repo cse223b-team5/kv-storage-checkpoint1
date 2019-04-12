2517e0610393f6ab180a98dabd60e6b1d6eef037
unknown <honglingbupt@gnail.com>
2019-04-11 17:53:24 -0700

    edit utils

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
-def load_config(self, config_path):
+def load_config(config_path):
     configs = {}
     with open(config_path) as f:
         line = f.readline()
         while line:
             item = line.rstrip().split(": ")
             if item[0] == "N":
-                self.nodes_count = int(item[1])
+                configs[item[0]] = int(item[1])
             else:
                 addrs = item[1].split(":")
                 configs[item[0]] = (addrs[0], addrs[1])
